1b928a1399cb81b497e42ade22fded25e3b66594
Andres <andres039039@hotmail.com>
2023-04-05 11:04:11 -0400

    improve card layout

--- components/projectCard.tsx
+++ components/projectCard.tsx
@@ -23,52 +23,46 @@ const ProjectCard = ({ project }: Project) => {
   const [hover, setHover] = useState(false);
 
   return (
-    // <Link href={project.href || ""}>
-      <Card
-        sx={{ cursor: "pointer", width: 300, height: 550 }}
-        raised={hover}
-        onMouseOver={() => setHover(true)}
-        onMouseOut={() => setHover(false)}
-      >
-        <Image
-          src={project.image}
-          height={200}
-          width={300}
-          alt="choosing an item"
-        />
-        <CardContent>
-          <Stack>
-            <Typography variant="h6" textAlign={"center"}>
-              {project.name}
-            </Typography>
-            <Container sx={{ mt: 3, height: "80%" }}>
-              <Typography paragraph>{project.description}</Typography>
-            </Container>
-            <Stack direction="row" sx={{height: "25%"}}>
-              {project.live && (
-                <Button
-                  size="small"
-                  onClick={() =>
-                    (window.location.href = project.live as string)
-                  }
-                >
-                  Live
-                </Button>
-              )}
-
+    <Card
+      sx={{ cursor: "pointer", width: 300, height: 500 }}
+      raised={hover}
+      onMouseOver={() => setHover(true)}
+      onMouseOut={() => setHover(false)}
+    >
+      <Image
+        src={project.image}
+        height={200}
+        width={300}
+        alt="choosing an item"
+      />
+      <CardContent>
+        <Stack>
+          <Typography variant="h6" textAlign={"center"}>
+            {project.name}
+          </Typography>
+          <Container sx={{ mt: 3, minHeight: "180px" }}>
+            <Typography paragraph>{project.description}</Typography>
+          </Container>
+          <Stack direction="row" sx={{ minHeight: "25%" }}>
+            {project.live && (
               <Button
                 size="small"
-                onClick={() =>
-                  (window.location.href = project.source as string)
-                }
+                onClick={() => (window.location.href = project.live as string)}
               >
-                Source Code
+                Live
               </Button>
-            </Stack>
+            )}
+
+            <Button
+              size="small"
+              onClick={() => (window.location.href = project.source as string)}
+            >
+              Source Code
+            </Button>
           </Stack>
-        </CardContent>
-      </Card>
-    // </Link>
+        </Stack>
+      </CardContent>
+    </Card>
   );
 };
 
